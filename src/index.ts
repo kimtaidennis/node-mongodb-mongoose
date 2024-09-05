1654c53b23f0af52da3834e95581bf2c8484599d
Dennis Kimtai <kimtai.developer@gmail.com>
2024-09-05 15:45:10 +0300

    add env variables

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json())
 // routes
 app.use('/api/products', productRoute)
 
-mongoose.connect('mongodb+srv://admin:wMRBTBZi0MTGtBPC@nodejs-mongodb.nofpy.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-mongodb').then( () => {
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@nodejs-mongodb.nofpy.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-mongodb`).then( () => {
     console.log("Connected to db");
     app.listen(port, () => {
         console.log(`[server]: Server is running at http://localhost:${port}`);
